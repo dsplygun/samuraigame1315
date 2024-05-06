0fd82acebe2cec579a6e5c56fb901c94d6e2e2a5
sixsixsixsixss <vladik.litovchenko2008@gmail.com>
2024-05-06 18:25:45 +0300

    v0.2

--- scripts/enemy_test.gd
+++ scripts/enemy_test.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
+@export var target : Node2D
 @export var enemy_speed = 100
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$PathfinderComponent.target = self.target
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,12 +12,13 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
-	var direction = (get_node("../best_character").position - position).normalized()
+	var direction = ($PathfinderComponent.direction).normalized()
 	velocity = direction * enemy_speed
 	
-	if (get_node("../best_character").position - position).length() < 125:
+	if $PathfinderComponent.distance_to_target() < 125:
 		velocity = Vector2(0,0)
-	
+		
+	print(position)
 	move_and_slide()
 	
 	#look_at( get_node("../shooter").position )
@@ -25,3 +27,7 @@ func _physics_process(delta):
 func _on_death_area_area_entered(area):
 	if area.is_in_group("wand"):
 		queue_free()
+
+
+func _on_health_component_out_of_health():
+	self.queue_free()
