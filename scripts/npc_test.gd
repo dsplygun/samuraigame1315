de84950f89ef5eda0a175a8cf023ce1fdce957bd
unknown <vladik.litovchenko2008@gmail.com>
2024-05-08 17:49:01 +0300

    v0.2.2

--- scripts/npc_test.gd
+++ scripts/npc_test.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-
+var give_item = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,8 +13,12 @@ func _process(delta):
 func _input(event):
 	if event is InputEventKey:
 		if $"../best_character" in $interact_area.get_overlapping_bodies():
-			if Input.is_action_just_pressed("interact"):
-				print("Hello world!")
+			if Input.is_action_just_pressed("interact") and give_item == false:
+				print("Here, take it!")
+				Inventory.add("npc_artifact")
+				give_item = true
+			elif Input.is_action_just_pressed("interact") and give_item == true:
+				print("You're already have it!")
 
 func _on_interact_area_body_entered(body):
 	pass
