ff1e0f929a7b6a90064b388559ad3f9a742f2fbc
Batruha1488 <150271889+Batruha1488@users.noreply.github.com>
2024-05-10 17:32:36 +0300

    Add files via upload

---
+++ script/KEYREG.gd
@@ -0,0 +1,24 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event is InputEventKey and  Input.get_action_strength("esc"):
+		if $"../game intr".visible:
+			$"../game intr".visible = false
+		else:
+			$"../game intr".visible = true
+			
+	if event is InputEventKey and  Input.get_action_strength("e"):
+		if $"../intr/ItemList".visible:
+			$"../intr/ItemList".visible = false
+		else:
+			$"../intr/ItemList".visible = true
--- scripts/npc_test.gd
+++ scripts/npc_test.gd
@@ -1,11 +1,9 @@
 extends CharacterBody2D
 
-@export var sprite : SpriteFrames 
 var give_item = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimatedSprite2D.sprite_frames = sprite
-	$AnimatedSprite2D.play("default")
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
