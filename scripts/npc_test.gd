db3a4d8aed66ca289465ff0aa1ec4a512a8d5554
dsplygun <150271009+dsplygun@users.noreply.github.com>
2024-05-10 17:41:20 +0300

    fix npc

--- scripts/npc_test.gd
+++ scripts/npc_test.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
+@export var sprite : SpriteFrames 
 var give_item = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AnimatedSprite2D.sprite_frames = sprite
+	$AnimatedSprite2D.play("default")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
