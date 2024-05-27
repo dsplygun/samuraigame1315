8012b11b5325bb37c8044b4cd20098d255eaebf2
unknown <vladik.litovchenko2008@gmail.com>
2024-05-27 17:53:58 +0300

    v0.2.4

--- scripts/best_character.gd
+++ scripts/best_character.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
+#var side = true
 
-@onready var char_anim = get_node("AnimatedSprite2D")
+@onready var char_anim = get_node("AnimationPlayer")
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -10,13 +11,25 @@ func _physics_process(delta):
 	var horizontal = Input.get_action_strength("move_right") - Input.get_action_strength("move_left") 
 	var direction = Vector2(horizontal, vertical)
 	
+	#if $attack_timer.is_stopped():
 	velocity = direction * 150
+		
 	
-	if velocity == Vector2(0,0):
+	if Input.get_action_strength("attack") or not $attack_timer.is_stopped():
+		if $attack_timer.is_stopped():
+			$attack_timer.start()
+		char_anim.play("attack_front")#+side
+	elif direction == Vector2(0,0):
 		char_anim.play("idle")
-	else:
-		char_anim.play("walk")
-	
+	elif direction == Vector2(0,1):
+		char_anim.play("walk_front")
+	elif direction == Vector2(0,-1):
+		char_anim.play("walk_back")
+	elif direction == Vector2(-1,0):
+		char_anim.play("walk_left")
+	elif direction == Vector2(1,0):
+		char_anim.play("walk_right")
+
 	move_and_slide()
 
 
