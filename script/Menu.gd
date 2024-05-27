355d49e1b337873706d95d88dfaa2cde1d2f6880
DanielFeil <d43266532@gmail.com>
2024-05-27 18:01:18 +0300

    menu

--- script/Menu.gd
+++ script/Menu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://levels/testlevel.tscn")
+	get_tree().change_scene_to_file("res://levels/forest.tscn"	)
 
 
 
