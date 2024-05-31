0064daba738238b75d3263f64b6d1a49389ef9c8
unknown <vladik.litovchenko2008@gmail.com>
2024-05-31 17:55:16 +0300

    v0.2.4.3

--- script/camera1.gd
+++ script/camera1.gd
@@ -10,4 +10,3 @@ func _ready():
 func _process(delta):
 	self.position.x = int($"../best_character".position.x / 576) * 576
 	self.position.y = int($"../best_character".position.y / 324) * 324
-	print(($"../best_character".position.x / 576))
--- scripts/best_character.gd
+++ scripts/best_character.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
-#var side = true
+
 var dash = 2
 var attack = 1
 @export var speed = 300
-@onready var char_anim = get_node("AnimationPlayer")
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+@onready var char_anim = get_node("AnimationTree")
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
@@ -13,35 +13,35 @@ func _physics_process(delta):
 	var horizontal = Input.get_action_strength("move_right") - Input.get_action_strength("move_left") 
 	var direction = Vector2(horizontal, vertical)
 	
-	#if $attack_timer.is_stopped():
 	velocity = direction * speed
-		
+	
+	if direction != Vector2(0,0):
+		$AnimationTree["parameters/idle/blend_position"] = direction
+		$AnimationTree["parameters/walk/blend_position"] = direction 
+		$AnimationTree["parameters/dash/blend_position"] = direction
+		if $attack_timer.is_stopped():
+			$AnimationTree["parameters/attack/blend_position"] = direction
+	
 	
 	if Input.is_action_just_pressed("attack") or not $attack_timer.is_stopped():
 		if $attack_timer.is_stopped() and attack > 0:
 			$attack_timer.start()
 			attack -= 1
+			speed = 0
 			$attack_kd.start()
-			char_anim.play("attack_front")#+side
+			char_anim.get("parameters/playback").travel("attack")
 	elif Input.get_action_strength("dash") or not $dash_anim.is_stopped():
 		if $dash_anim.is_stopped() and dash > 0:
 			speed = 600
 			$dash_anim.start()
 			dash -= 1
 			$dash_timer.start()
-
-			char_anim.play("dash_right")
+			char_anim.get("parameters/playback").travel("dash")
 			
-	elif direction == Vector2(0,0):
-		char_anim.play("idle")
-	elif direction == Vector2(0,1):
-		char_anim.play("walk_front")
-	elif direction == Vector2(0,-1):
-		char_anim.play("walk_back")
-	elif direction == Vector2(-1,0):
-		char_anim.play("walk_left")
-	elif direction == Vector2(1,0):
-		char_anim.play("walk_right")
+	elif velocity == Vector2(0,0):
+		char_anim.get("parameters/playback").travel("idle")
+	elif velocity.length() > 0:
+		char_anim.get("parameters/playback").travel("walk")
 
 	move_and_slide()
 
@@ -65,3 +65,7 @@ func _on_freeze_timer_timeout():
 
 func _on_attack_kd_timeout():
 	attack = 1
+
+
+func _on_attack_timer_timeout():
+	speed = 300
