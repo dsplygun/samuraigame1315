68134fb122c061990fca222bae9ed3274d926c0f
DanielFeil <d43266532@gmail.com>
2024-06-05 18:08:41 +0300

    v.0.0.4
    Level Update

--- script/camera1.gd
+++ script/camera1.gd
@@ -8,5 +8,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+<<<<<<< Updated upstream
 	self.position.x = int($"../best_character".position.x / 576) * 576
 	print(($"../best_character".position.x / 576))
+=======
+	var new_pos_x= int($"../best_character".position.x / 576) * 576
+	var new_pos_y= int($"../best_character".position.y / 324) * 324
+	if new_pos_x != self.position.x or new_pos_y !=self.position.y:
+		var new_tween=create_tween()
+		new_tween.tween_property(self,"position",Vector2(new_pos_x,new_pos_y),0.5)
+		
+>>>>>>> Stashed changes
