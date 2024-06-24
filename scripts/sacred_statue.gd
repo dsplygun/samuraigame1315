a5d17522f5476b80095a9814f2078d56243afac3
DanielFeil <d43266532@gmail.com>
2024-06-24 17:58:48 +0300

    сундук и раставление врагов

--- scripts/sacred_statue.gd
+++ scripts/sacred_statue.gd
@@ -1,16 +1,19 @@
 extends StaticBody2D
+@export var one_use : bool = false
+var can_use = true
 @export var effect : Resource
 #@export var value : Array[Variant]
 func _process(delta):
 	pass
 
 func _input(event):
-	if event is InputEventKey:
+	if event is InputEventKey and can_use:
 		if Input.is_action_just_pressed("interact"):
 			for body in $interact_area.get_overlapping_bodies():
 				if body.is_in_group("hero"):
 					effect.activate_effect(body)
-					
+					if one_use:
+						can_use = false
 					
 
 func _on_interact_area_body_entered(body):
